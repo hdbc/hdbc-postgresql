a760beb50298bff5ca6c891190c79623070ccd0d
John Goerzen <jgoerzen@complete.org>
2005-12-18 00:52:34 +0100

    Reverted debugging

--- hdbc-sqlite3-helper.h
+++ hdbc-sqlite3-helper.h
@@ -2,7 +2,3 @@
 
 extern int sqlite3_bind_text2(sqlite3_stmt* a, int b, const char *c, int d);
 
-extern int sqlite3_prepare_dbg(sqlite3 *db, const char *zSql, int nBytes,
-                               sqlite3_stmt **ppStmt,
-                               const char **pzTail);
-
