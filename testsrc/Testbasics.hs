fe1887d89e6bd7e64ee0c5c529d8eef7e6a79baa
John Goerzen <jgoerzen@complete.org>
2005-12-22 00:17:06 +0100

    Also update tests for latest HDBC

--- testsrc/Testbasics.hs
+++ testsrc/Testbasics.hs
@@ -19,30 +19,30 @@ multiFinish = dbTestCase (\dbh ->
 basicQueries = dbTestCase (\dbh ->
     do sth <- prepare dbh "SELECT 1 + 1"
        sExecute sth []
-       fetchRow sth >>= (assertEqual "row 1" (Just [Just "2"]))
-       fetchRow sth >>= (assertEqual "last row" Nothing)
+       sFetchRow sth >>= (assertEqual "row 1" (Just [Just "2"]))
+       sFetchRow sth >>= (assertEqual "last row" Nothing)
                           )
     
 createTable = dbTestCase (\dbh ->
-    do run dbh "CREATE TABLE test1 (testname VARCHAR(20), testid INTEGER, testint INTEGER, testtext TEXT)" []
+    do sRun dbh "CREATE TABLE test1 (testname VARCHAR(20), testid INTEGER, testint INTEGER, testtext TEXT)" []
        commit dbh
                          )
 
 dropTable = dbTestCase (\dbh ->
-    do run dbh "DROP TABLE test1" []
+    do sRun dbh "DROP TABLE test1" []
        commit dbh
                        )
 
 runReplace = dbTestCase (\dbh ->
-    do run dbh "INSERT INTO test1 VALUES (?, ?, ?, ?)" r1
-       run dbh "INSERT INTO test1 VALUES (?, ?, 2, ?)" r2
+    do sRun dbh "INSERT INTO test1 VALUES (?, ?, ?, ?)" r1
+       sRun dbh "INSERT INTO test1 VALUES (?, ?, 2, ?)" r2
        commit dbh
        sth <- prepare dbh "SELECT * FROM test1 WHERE testname = 'runReplace' ORDER BY testid"
        sExecute sth []
-       fetchRow sth >>= (assertEqual "r1" (Just r1))
-       fetchRow sth >>= (assertEqual "r2" (Just [Just "runReplace", Just "2",
+       sFetchRow sth >>= (assertEqual "r1" (Just r1))
+       sFetchRow sth >>= (assertEqual "r2" (Just [Just "runReplace", Just "2",
                                                  Just "2", Nothing]))
-       fetchRow sth >>= (assertEqual "lastrow" Nothing)
+       sFetchRow sth >>= (assertEqual "lastrow" Nothing)
                        )
     where r1 = [Just "runReplace", Just "1", Just "1234", Just "testdata"]
           r2 = [Just "runReplace", Just "2", Nothing]
@@ -54,13 +54,13 @@ executeReplace = dbTestCase (\dbh ->
        commit dbh
        sth <- prepare dbh "SELECT * FROM test1 WHERE testname = ? ORDER BY testid"
        sExecute sth [Just "executeReplace"]
-       fetchRow sth >>= (assertEqual "r1" 
+       sFetchRow sth >>= (assertEqual "r1" 
                          (Just $ map Just ["executeReplace", "1", "1234", 
                                            "Foo"]))
-       fetchRow sth >>= (assertEqual "r2"
+       sFetchRow sth >>= (assertEqual "r2"
                          (Just [Just "executeReplace", Just "2", Nothing,
                                 Just "Bar"]))
-       fetchRow sth >>= (assertEqual "lastrow" Nothing)
+       sFetchRow sth >>= (assertEqual "lastrow" Nothing)
                             )
 
 executeMany = dbTestCase (\dbh ->
@@ -69,8 +69,8 @@ executeMany = dbTestCase (\dbh ->
        commit dbh
        sth <- prepare dbh "SELECT testid, testint, testtext FROM test1 WHERE testname = 'multi'"
        sExecute sth []
-       mapM_ (\r -> fetchRow sth >>= (assertEqual "" (Just r))) rows
-       fetchRow sth >>= (assertEqual "lastrow" Nothing)
+       mapM_ (\r -> sFetchRow sth >>= (assertEqual "" (Just r))) rows
+       sFetchRow sth >>= (assertEqual "lastrow" Nothing)
                           )
     where rows = [map Just ["1", "1234", "foo"],
                   map Just ["2", "1341", "bar"],
