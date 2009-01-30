85ea5c96af09911f20cf330f75d2cd13510d88f6
John Goerzen <jgoerzen@complete.org>
2009-01-30 09:19:55 -0600

    Infrastructure updates for date/time tests

--- testsrc/SpecificDB.hs
+++ testsrc/SpecificDB.hs
@@ -6,3 +6,15 @@ import Test.HUnit
 
 connectDB = 
     handleSqlError (connectPostgreSQL "")
+
+dateTimeTypeOfSqlValue :: SqlValue -> String
+dateTimeTypeOfSqlValue (SqlLocalDate _) = "date"
+dateTimeTypeOfSqlValue (SqlLocalTimeOfDay _) = "time without time zone"
+dateTimeTypeOfSqlValue (SqlLocalTime _) = "timestamp without time zone"
+dateTimeTypeOfSqlValue (SqlZonedTime _) = "timestamp with time zone"
+dateTimeTypeOfSqlValue (SqlUTCTime _) = "timestamp without time zone"
+dateTimeTypeOfSqlValue (SqlDiffTime _) = "interval"
+dateTimeTypeOfSqlValue (SqlPOSIXTime _) = "integer"
+dateTimeTypeOfSqlValue (SqlEpochTime _) = "integer"
+dateTimeTypeOfSqlValue (SqlTimeDiff _) = "interval"
+dateTimeTypeOfSqlValue _ = "text"
