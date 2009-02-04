00d20d30267c6ecb3bad84e9c93c322849bdca4c
John Goerzen <jgoerzen@complete.org>
2009-02-04 08:43:09 -0600

    Now test times with fractional seconds

--- testsrc/TestTime.hs
+++ testsrc/TestTime.hs
@@ -16,46 +16,22 @@ instance Eq ZonedTime where
     a == b = zonedTimeToUTC a == zonedTimeToUTC b &&
              zonedTimeZone a == zonedTimeZone b
 
-baseDay :: Day
-baseDay = localDay baseLocalTime
-
-baseTimeOfDay :: TimeOfDay
-baseTimeOfDay = localTimeOfDay baseLocalTime
-
-baseZonedTimeOfDay :: (TimeOfDay, TimeZone)
-baseZonedTimeOfDay = fromSql (SqlZonedTime baseZonedTime)
-
-baseLocalTime :: LocalTime
-baseLocalTime = zonedTimeToLocalTime baseZonedTime
-
-baseZonedTime :: ZonedTime
-baseZonedTime = fromJust $ parseTime defaultTimeLocale (iso8601DateFormat (Just "%T %z"))
+testZonedTime :: ZonedTime
+testZonedTime = fromJust $ parseTime defaultTimeLocale (iso8601DateFormat (Just "%T %z"))
                  "1989-08-01 15:33:01 -0500"
 
-baseUTCTime :: UTCTime
-baseUTCTime = convert baseZonedTime
+testZonedTimeFrac :: ZonedTime
+testZonedTimeFrac = fromJust $ parseTime defaultTimeLocale (iso8601DateFormat (Just "%T%Q %z"))
+                    "1989-08-01 15:33:01.536 -0500"
 
-baseDiffTime :: NominalDiffTime
-baseDiffTime = basePOSIXTime
-
-basePOSIXTime :: POSIXTime
-basePOSIXTime = convert baseZonedTime
-
-baseClockTime :: ST.ClockTime
-baseClockTime = convert baseZonedTime
-
-baseCalendarTime :: ST.CalendarTime
-baseCalendarTime = convert baseZonedTime
-
-baseTimeDiff :: ST.TimeDiff
-baseTimeDiff = convert baseDiffTime
 
 rowdata t = [[SqlInt32 100, toSql t, SqlNull]]
 
 testDTType inputdata convToSqlValue = dbTestCase $ \dbh ->
     do run dbh ("CREATE TABLE hdbctesttime (testid INTEGER PRIMARY KEY NOT NULL, \
                 \testvalue " ++ dateTimeTypeOfSqlValue value ++ ")") []
-       finally (testIt dbh) (do run dbh "DROP TABLE hdbctesttime" []
+       finally (testIt dbh) (do commit dbh
+                                run dbh "DROP TABLE hdbctesttime" []
                                 commit dbh
                             )
     where testIt dbh =
@@ -72,15 +48,50 @@ testDTType inputdata convToSqlValue = dbTestCase $ \dbh ->
 mkTest label inputdata convfunc =
     TestLabel label (testDTType inputdata convfunc)
 
-tests = TestList [mkTest "Day" baseDay toSql,
-                  mkTest "TimeOfDay" baseTimeOfDay toSql,
-                  mkTest "ZonedTimeOfDay" baseZonedTimeOfDay toSql,
-                  mkTest "LocalTime" baseLocalTime toSql,
-                  mkTest "ZonedTime" baseZonedTime toSql,
-                  mkTest "UTCTime" baseUTCTime toSql,
-                  mkTest "DiffTime" baseDiffTime toSql,
-                  mkTest "POSIXTime" basePOSIXTime posixToSql,
-                  mkTest "ClockTime" baseClockTime toSql,
-                  mkTest "CalendarTime" baseCalendarTime toSql,
-                  mkTest "TimeDiff" baseTimeDiff toSql
-                 ]
+tests = TestList $
+    ((TestLabel "Non-frac" $ testIt testZonedTime) :
+     if supportsFracTime then [TestLabel "Frac" $ testIt testZonedTimeFrac] else [])
+
+testIt baseZonedTime = 
+    TestList [mkTest "Day" baseDay toSql,
+              mkTest "TimeOfDay" baseTimeOfDay toSql,
+              mkTest "ZonedTimeOfDay" baseZonedTimeOfDay toSql,
+              mkTest "LocalTime" baseLocalTime toSql,
+              mkTest "ZonedTime" baseZonedTime toSql,
+              mkTest "UTCTime" baseUTCTime toSql,
+              mkTest "DiffTime" baseDiffTime toSql,
+              mkTest "POSIXTime" basePOSIXTime posixToSql,
+              mkTest "ClockTime" baseClockTime toSql,
+              mkTest "CalendarTime" baseCalendarTime toSql,
+              mkTest "TimeDiff" baseTimeDiff toSql
+             ]
+    where 
+      baseDay :: Day
+      baseDay = localDay baseLocalTime
+
+      baseTimeOfDay :: TimeOfDay
+      baseTimeOfDay = localTimeOfDay baseLocalTime
+
+      baseZonedTimeOfDay :: (TimeOfDay, TimeZone)
+      baseZonedTimeOfDay = fromSql (SqlZonedTime baseZonedTime)
+
+      baseLocalTime :: LocalTime
+      baseLocalTime = zonedTimeToLocalTime baseZonedTime
+
+      baseUTCTime :: UTCTime
+      baseUTCTime = convert baseZonedTime
+
+      baseDiffTime :: NominalDiffTime
+      baseDiffTime = basePOSIXTime
+
+      basePOSIXTime :: POSIXTime
+      basePOSIXTime = convert baseZonedTime
+
+      baseClockTime :: ST.ClockTime
+      baseClockTime = convert baseZonedTime
+
+      baseCalendarTime :: ST.CalendarTime
+      baseCalendarTime = convert baseZonedTime
+
+      baseTimeDiff :: ST.TimeDiff
+      baseTimeDiff = convert baseDiffTime
